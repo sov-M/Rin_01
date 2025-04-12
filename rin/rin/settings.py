d9dce560b4b20e5fdbab0d1644195118431000c1
Maksim Sovetkin <sovetkin.maksim@mail.ru>
2025-04-13 00:24:39 +0400

    add docker

--- rin/rin/settings.py
+++ rin/rin/settings.py
@@ -122,14 +122,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-
-STATICFULES_DIRS = [
-    BASE_DIR / "static",
-    'var/www/static',
-]
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # Для collectstatic
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # Куда collectstatic копирует файлы
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'main/static')]  # Где исходные файлы
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- rin/rin/urls.py
+++ rin/rin/urls.py
@@ -6,8 +6,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("main.urls")),
-    path("products", include("products.urls")),
-]
-# + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path("products/", include("products.urls")),  # Добавил слеш в конец
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
+  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

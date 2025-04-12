a0481fee55fe2d4d017598b021c1c0b7d96efe1b
Maksim Sovetkin <sovetkin.maksim@mail.ru>
2025-04-13 00:23:13 +0400

    add docker

--- rin/main/urls.py
+++ rin/main/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from .import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("", views.index, name="home"),
@@ -9,4 +11,5 @@ urlpatterns = [
     path("login/", views.login, name="login"),
     path("logout/", views.logout, name="logout"),
     path("test/", views.test, name="test"),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
--- rin/products/urls.py
+++ rin/products/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("", views.products_home, name="products_home"),
@@ -7,4 +9,4 @@ urlpatterns = [
     path("<int:pk>/", views.ProductsDetailView.as_view(), name="products-detail"),
     path("<int:pk>/update/", views.ProductsUpdateView.as_view(), name="products-update"),
     path("<int:pk>/delete/", views.ProductsDeleteView.as_view(), name="products-delete"),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
